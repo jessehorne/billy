51c08aaaac6e1ae18af96b45b37f289708c1040c
Jesse Horne <j.horne2796@gmail.com>
2015-09-17 16:53:57 -0400

    initial commit

---
+++ app.php
@@ -0,0 +1,33 @@
+<?php
+
+include("./Billy/Game.php");
+
+class MyGame extends Game {
+
+    function __construct() {
+        parent::__construct();
+
+        echo "Your minimal game has been started.\n";
+
+        $this->init();
+    }
+
+    protected function update($dt) {
+
+    }
+
+    protected function draw() {
+        Game::print_string("You have successfully created a game! Now get to work.", 100, 100);
+    }
+
+    protected function key_pressed($key) {
+
+    }
+
+    protected function key_released($key) {
+
+    }
+
+}
+
+$game = new MyGame();
